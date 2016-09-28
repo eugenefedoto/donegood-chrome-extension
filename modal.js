3f2a80e5b6d07a107455fea5e21f58c6c3d11ce9
Eugene Fedotov <eugenefedoto@gmail.com>
2016-09-28 00:22:31 -0400

    Add BootStrap to modal and rewrite to jQuery

--- modal.js
+++ modal.js
@@ -1,36 +1,19 @@
-wrapperDiv = document.createElement("div");
-wrapperDiv.setAttribute("style","position: absolute; left: 0px; top: 0px; background-color: rgb(255, 255, 255); opacity: 0.5; z-index: 2000; height: 1083px; width: 100%;");
-
-iframeElement = document.createElement("iframe");
-iframeElement.setAttribute("style","width: 100%; height: 100%;");
-
-wrapperDiv.appendChild(iframeElement);
-
-modalDialogParentDiv = document.createElement("div");
-modalDialogParentDiv.setAttribute("style","position: absolute; width: 350px; border: 1px solid rgb(51, 102, 153); padding: 10px; background-color: rgb(255, 255, 255); z-index: 2001; overflow: auto; text-align: center; top: 149px;");
-
-modalDialogSiblingDiv = document.createElement("div");
-
-modalDialogTextDiv = document.createElement("div"); 
-modalDialogTextDiv.setAttribute("style" , "text-align:center");
-
-breakElement = document.createElement("br"); 
-imageElement = document.createElement("img"); 
-imageElement.src = chrome.extension.getURL("visit-donegood.jpg");
-
-modalDialogAnchor = document.createElement("a");
-modalDialogAnchor.setAttribute("href", "http://donegood.co");
-modalDialogText = document.createElement("strong"); 
-modalDialogText.innerHTML = "Visit DoneGood.co";
-
-modalDialogAnchor.appendChild(modalDialogText);
-modalDialogTextDiv.appendChild(modalDialogAnchor);
-modalDialogTextDiv.appendChild(breakElement);
-modalDialogTextDiv.appendChild(breakElement);
-modalDialogTextDiv.appendChild(imageElement);
-
-modalDialogSiblingDiv.appendChild(modalDialogTextDiv);
-modalDialogParentDiv.appendChild(modalDialogSiblingDiv);
-
-document.body.appendChild(wrapperDiv);
-document.body.appendChild(modalDialogParentDiv);
+$(function () {
+    var imgSrc = chrome.extension.getURL('visit-donegood.jpg');
+    
+    $('body').append(`<div id="myModal" class="modal fade" tabindex="-1" role="dialog">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-body">
+        <p style="text-align: center;"><a href="http://donegood.co/"><strong>Visit DoneGood.co</strong></a></p>
+        <img width="100px" height="100px" style="display: block;
+  margin-left: auto;
+  margin-right: auto;" src="${imgSrc}">
+      </div>
+    </div><!-- /.modal-content -->
+  </div><!-- /.modal-dialog -->
+</div><!-- /.modal -->`
+);
+    
+    $('#myModal').modal();
+});
